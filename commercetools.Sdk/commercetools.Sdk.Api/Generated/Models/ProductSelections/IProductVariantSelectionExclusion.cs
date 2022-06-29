942fe8d2a54a0cfa9b9df52c5f3318dc0fa64a50
Auto Mation <automation@commercetools.com>
2022-06-29 11:48:29 +0000

    TASK: Updating SDK

--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/ProductSelections/IProductVariantSelectionExclusion.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/ProductSelections/IProductVariantSelectionExclusion.cs
@@ -7,6 +7,8 @@ namespace commercetools.Sdk.Api.Models.ProductSelections
     [DeserializeAs(typeof(commercetools.Sdk.Api.Models.ProductSelections.ProductVariantSelectionExclusion))]
     public partial interface IProductVariantSelectionExclusion : IProductVariantSelection
     {
+        new IProductVariantSelectionTypeEnum Type { get; set; }
+
         List<string> Skus { get; set; }
     }
 }
--- commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/ProductSelections/IProductVariantSelectionInclusion.cs
+++ commercetools.Sdk/commercetools.Sdk.Api/Generated/Models/ProductSelections/IProductVariantSelectionInclusion.cs
@@ -7,6 +7,8 @@ namespace commercetools.Sdk.Api.Models.ProductSelections
     [DeserializeAs(typeof(commercetools.Sdk.Api.Models.ProductSelections.ProductVariantSelectionInclusion))]
     public partial interface IProductVariantSelectionInclusion : IProductVariantSelection
     {
+        new IProductVariantSelectionTypeEnum Type { get; set; }
+
         List<string> Skus { get; set; }
     }
 }
